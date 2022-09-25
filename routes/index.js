f5b52b90b556e02473f59a4345c1cca5901a62d6
Jerry Kuo <narikuo@gmail.com>
2022-09-25 16:34:36 -0400

    create and build pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about-me', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+router.get('/contact-me', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
