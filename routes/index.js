967be917547a457beabe5622e5a454ddb668f8ab
Jerry Kuo <narikuo@gmail.com>
2022-09-30 12:53:14 -0400

    change the template of contact page

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/services', function(req, res, next) {
 });
 
 router.get('/contact-me', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact-me', { title: 'Contact Me' });
 });
 
 //Get form data from contact me
