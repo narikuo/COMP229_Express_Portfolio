7fd1a25904edf17731bb5f1e457a48a2f6d94d93
Jerry Kuo <narikuo@gmail.com>
2022-09-30 11:45:29 -0400

    update contact form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var parseUrl = require('body-parser')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -21,5 +23,15 @@ router.get('/services', function(req, res, next) {
 router.get('/contact-me', function(req, res, next) {
   res.render('index', { title: 'Contact Me' });
 });
+
+//Get form data from contact me
+router.post('/', function(req, res, next) {
+  console.log('-----> Message Form request: \nName: ', req.body.yourNameInput,
+                ', Mail: ', req.body.yourMailInput,
+                ', Phone: ', req.body.yourPhoneInput,
+                ', \nMessage: ', req.body.messageTextarea)
+  res.render('index', { title: 'Home' });
+  
+})
 
 module.exports = router;
